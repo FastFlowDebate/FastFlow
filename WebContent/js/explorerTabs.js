b9fb0d7c1a659c810738d873ede1e5f545395c62
Adam Towers <adam.towers@icloud.com>
2015-11-15 21:52:04 -0800

    weird tab stuff doesn't work

---
+++ WebContent/js/explorerTabs.js
@@ -0,0 +1,20 @@
+'use strict';
+$('#tab Cards').click(function (e) {
+    e.preventDefault();
+    $(this).tab('show');
+});
+
+$('#tab Blocks').click(function (e) {
+    e.preventDefault();
+    $(this).tab('show');
+});
+
+$('#tab Starred').click(function (e) {
+    e.preventDefault();
+    $(this).tab('show');
+});
+
+$('#tab Settings').click(function (e) {
+    e.preventDefault();
+    $(this).tab('show');
+});
