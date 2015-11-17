5fe13aed2d070df36a3a46654c96c3fc2cc50c49
Isaac Lo <isaaclo123@gmail.com>
2015-11-16 23:26:52 -0800

    added experimental simpler text editor

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 // app.js
-var express = require('express');  
-var app = express();  
-var server = require('http').createServer(app);  
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 
-app.use(express.static(__dirname + '/WebContent/'));  
-app.get('/', function(req, res,next) {  
+app.use(express.static(__dirname + '/WebContent/'));
+app.get('/', function(req, res,next) {
     res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(client) {  
+io.on('connection', function(client) {
     console.log('Client connected...');
 
     client.on('join', function(data) {
@@ -23,4 +23,4 @@ io.on('connection', function(client) {
 
 });
 
-server.listen(4200);  
+server.listen(4200);
