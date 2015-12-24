337683df6d0aec4f7312ca0fda91a3fc1ee1bbe1
Zarkoix <adam.towers@icloud.com>
2015-12-24 11:58:50 -0800

    trying to get stuff to work

--- README.md
+++ README.md
@@ -1,23 +1,27 @@
-
-
 # FastFlow
 
+Debate organization suite for making High School/Collegiate debate much for effeicient.
+* installation
 
+      npm install
 
-## Usage
-at the moment all libraries are got via cdn, but they are in package.json, to install locally use:
+* running
 
-> npm init
+      npm start
 
+* or (in seperate tabs, for using hot loading)
 
+      npm run hot-server
+      npm run start-hot
 
-## Developing
-Being developed using NodeJS, AngularJS, TextAngular, Java, Pythong.. We have big stack
+See [electron-react-boilerplate](https://github.com/chentsulin/electron-react-boilerplate) for additional information about how the non-/app components work.
 
+## Contribution
 
-### Tools
+* Use the [Javascript Standard code style](https://github.com/feross/standard) for any and all JS development
 
-Created with [Nodeclipse](https://github.com/Nodeclipse/nodeclipse-1)
- ([Eclipse Marketplace](http://marketplace.eclipse.org/content/nodeclipse), [site](http://www.nodeclipse.org))   
+* Try to keep the CHANGELOG.md updated, especially for features, improvements, and bug fixes.
 
-Nodeclipse is free open-source project that grows with your contributions.
+* Try to keep package.json as clean as possible, use devDependencies over dependencies when ever possible.
+
+* Update this README with any installation/building changes. 
--- server.js
+++ server.js
@@ -1,64 +1,34 @@
-// app.js
-var express = require('express');
-var app = express();
-var server = require('http').createServer(app);
-var io = require('socket.io')(server);
+/* eslint strict: 0, no-console: 0 */
+'use strict';
 
-app.use(express.static(__dirname + '/WebContent/'));
-app.get('/', function(req, res,next) {
-    res.sendFile(__dirname + '/index.html');
+const path = require('path');
+const express = require('express');
+const webpack = require('webpack');
+const config = require('./webpack.config.development');
+
+const app = express();
+const compiler = webpack(config);
+
+const PORT = 3000;
+
+app.use(require('webpack-dev-middleware')(compiler, {
+  publicPath: config.output.publicPath,
+  stats: {
+    colors: true
+  }
+}));
+
+app.use(require('webpack-hot-middleware')(compiler));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'app', 'hot-dev-app.html'));
 });
 
-io.on('connection', function(client) {
-    console.log('Client connected...');
+app.listen(PORT, 'localhost', err => {
+  if (err) {
+    console.log(err);
+    return;
+  }
 
-    client.on('join', function(data) {
-        console.log(data);
-    });
-    client.on('newCard', function(data) {
-    	console.log("card{ tag: " + data.tag + ", cite: " + data.cite + ", content: " + data.content + "}");
-    	//do stuff with data, at the moment doesn't print data properly lol
-    	//TODO parse data - card content and save to new card then return cardSaved(cardID);
-      var PythonShell = require('python-shell');
-      var filesavepy = new PythonShell('backend/filesave.py');
-      var indexShell = new PythonShell('backend/tagindex.py');
-
-      //var args = process.argv.slice(4);
-      var tags;
-      var content;
-      var location;
-      var filename;
-
-      function filesave(tags, content, location, filename) {
-        filesavepy.send(tags);
-        console.log(tags);
-        //content
-        filesavepy.send(content);
-        console.log(content);
-        //folder location
-        filesavepy.send(location);
-        console.log(location);
-        //filename
-        filesavepy.send(filename);
-        console.log(filename);
-
-        indexShell.send(location);
-        console.log(location);
-
-        indexShell.send(location);
-        console.log(location);
-        //indexShell.send(location);
-
-        indexShell.end(function (err) {
-          if (err) throw err;
-          console.log('finished');
-        });
-
-      }
-
-      filesave(data.tag.toString(), "ugh", "backend/testfolder", data.cite.toString());
-
+  console.log(`Listening at http://localhost:${PORT}`);
 });
-});
-
-server.listen(4200);
