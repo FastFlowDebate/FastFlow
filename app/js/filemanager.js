ddff4c972bdf8cbf07d0c9b9ff9a37d2dc1af0f8
Zarkoix <adam.towers@icloud.com>
2016-01-03 19:20:52 -0800

    code cleanup

--- app/js/filemanager.js
+++ app/js/filemanager.js
@@ -1,21 +1,21 @@
-const ipcRenderer = require('electron').ipcRenderer;
+const ipcRenderer = require('electron').ipcRenderer
 
-$( document ).ready(function() {
-  var dataJSON = ipcRenderer.sendSync('FileManager', 'ready');
+$(document).ready(function () {
+  var dataJSON = ipcRenderer.sendSync('FileManager', 'ready')
 
   for (i = 0; i < dataJSON[0].length; i++) {
-    var element = document.createElement("UL");
-    var text = document.createTextNode(dataJSON[0][i]);
-    element.appendChild(text);
-    element.id = String(i);
-    document.getElementById("thefilemanager").appendChild(element);
+    var element = document.createElement('UL')
+    var text = document.createTextNode(dataJSON[0][i])
+    element.appendChild(text)
+    element.id = String(i)
+    document.getElementById('thefilemanager').appendChild(element)
 
 
     for (j = 0; j < dataJSON[1][i].length; j++){
-      var element1 = document.createElement("LI");
-      var text1 = document.createTextNode(dataJSON[1][i][j]);
-      element1.appendChild(text1);
-      document.getElementById(String(i)).appendChild(element1);
+      var element1 = document.createElement('LI')
+      var text1 = document.createTextNode(dataJSON[1][i][j])
+      element1.appendChild(text1)
+      document.getElementById(String(i)).appendChild(element1)
     }
   }
-});
+})
--- main.js
+++ main.js
@@ -1,53 +1,50 @@
 /* eslint strict: 0 */
-'use strict';
+'use strict'
 
-var path = require('path');
-var fs = require('fs');
+var path = require('path')
+var fs = require('fs')
 
-var PythonShell = require('python-shell');
-const electron = require('electron');
-const app = electron.app;
-const BrowserWindow = electron.BrowserWindow;
-const Menu = electron.Menu;
-const crashReporter = electron.crashReporter;
-const shell = electron.shell;
-const ipcMain = require('electron').ipcMain;
-let menu;
-let template;
-let mainWindow = null;
+var PythonShell = require('python-shell')
+const electron = require('electron')
+const app = electron.app
+const BrowserWindow = electron.BrowserWindow
+const Menu = electron.Menu
+const crashReporter = electron.crashReporter
+const shell = electron.shell
+const ipcMain = require('electron').ipcMain
+let menu
+let template
+let mainWindow = null
 
-crashReporter.start();
+crashReporter.start()
 
 if (process.env.NODE_ENV === 'development') {
-  require('electron-debug')();
+  require('electron-debug')()
 }
 
-
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') app.quit();
-});
-
+  if (process.platform !== 'darwin') app.quit()
+})
 
 app.on('ready', () => {
-
-
-
-  mainWindow = new BrowserWindow({ width: 1024, height: 728 });
+  mainWindow = new BrowserWindow({
+    width: 1024,
+    height: 728
+  })
 
   if (process.env.HOT) {
-    mainWindow.loadURL(`file://${__dirname}/app/hot-dev-app.html`);
+    mainWindow.loadURL(`file://${__dirname}/app/hot-dev-app.html`)
   } else {
-    mainWindow.loadURL(`file://${__dirname}/app/app.html`);
+    mainWindow.loadURL(`file://${__dirname}/app/app.html`)
   }
 
   mainWindow.on('closed', () => {
-    mainWindow = null;
-  });
+    mainWindow = null
+  })
 
   if (process.env.NODE_ENV === 'development') {
-    mainWindow.openDevTools();
+    mainWindow.openDevTools()
   }
-
 
   if (process.platform === 'darwin') {
     template = [{
@@ -78,8 +75,8 @@ app.on('ready', () => {
       }, {
         label: 'Quit',
         accelerator: 'Command+Q',
-        click() {
-          app.quit();
+        click () {
+          app.quit()
         }
       }]
     }, {
@@ -116,26 +113,26 @@ app.on('ready', () => {
       submenu: (process.env.NODE_ENV === 'development') ? [{
         label: 'Reload',
         accelerator: 'Command+R',
-        click() {
-          mainWindow.restart();
+        click () {
+          mainWindow.restart()
         }
       }, {
         label: 'Toggle Full Screen',
         accelerator: 'Ctrl+Command+F',
-        click() {
-          mainWindow.setFullScreen(!mainWindow.isFullScreen());
+        click () {
+          mainWindow.setFullScreen(!mainWindow.isFullScreen())
         }
       }, {
         label: 'Toggle Developer Tools',
         accelerator: 'Alt+Command+I',
-        click() {
-          mainWindow.toggleDevTools();
+        click () {
+          mainWindow.toggleDevTools()
         }
       }] : [{
         label: 'Toggle Full Screen',
         accelerator: 'Ctrl+Command+F',
-        click() {
-          mainWindow.setFullScreen(!mainWindow.isFullScreen());
+        click () {
+          mainWindow.setFullScreen(!mainWindow.isFullScreen())
         }
       }]
     }, {
@@ -158,29 +155,29 @@ app.on('ready', () => {
       label: 'Help',
       submenu: [{
         label: 'Learn More',
-        click() {
-          shell.openExternal('http://electron.atom.io');
+        click () {
+          shell.openExternal('http://electron.atom.io')
         }
       }, {
         label: 'Documentation',
-        click() {
-          shell.openExternal('https://github.com/atom/electron/tree/master/docs#readme');
+        click () {
+          shell.openExternal('https://github.com/atom/electron/tree/master/docs#readme')
         }
       }, {
         label: 'Community Discussions',
-        click() {
-          shell.openExternal('https://discuss.atom.io/c/electron');
+        click () {
+          shell.openExternal('https://discuss.atom.io/c/electron')
         }
       }, {
         label: 'Search Issues',
-        click() {
-          shell.openExternal('https://github.com/atom/electron/issues');
+        click () {
+          shell.openExternal('https://github.com/atom/electron/issues')
         }
       }]
-    }];
+    }]
 
-    menu = Menu.buildFromTemplate(template);
-    Menu.setApplicationMenu(menu);
+    menu = Menu.buildFromTemplate(template)
+    Menu.setApplicationMenu(menu)
   } else {
     template = [{
       label: '&File',
@@ -190,8 +187,8 @@ app.on('ready', () => {
       }, {
         label: '&Close',
         accelerator: 'Ctrl+W',
-        click() {
-          mainWindow.close();
+        click () {
+          mainWindow.close()
         }
       }]
     }, {
@@ -199,153 +196,129 @@ app.on('ready', () => {
       submenu: (process.env.NODE_ENV === 'development') ? [{
         label: '&Reload',
         accelerator: 'Ctrl+R',
-        click() {
-          mainWindow.restart();
+        click () {
+          mainWindow.restart()
         }
       }, {
         label: 'Toggle &Full Screen',
         accelerator: 'F11',
-        click() {
-          mainWindow.setFullScreen(!mainWindow.isFullScreen());
+        click () {
+          mainWindow.setFullScreen(!mainWindow.isFullScreen())
         }
       }, {
         label: 'Toggle &Developer Tools',
         accelerator: 'Alt+Ctrl+I',
-        click() {
-          mainWindow.toggleDevTools();
+        click () {
+          mainWindow.toggleDevTools()
         }
       }] : [{
         label: 'Toggle &Full Screen',
         accelerator: 'F11',
-        click() {
-          mainWindow.setFullScreen(!mainWindow.isFullScreen());
+        click () {
+          mainWindow.setFullScreen(!mainWindow.isFullScreen())
         }
       }]
     }, {
       label: 'Help',
       submenu: [{
         label: 'Learn More',
-        click() {
-          shell.openExternal('http://electron.atom.io');
+        click () {
+          shell.openExternal('http://electron.atom.io')
         }
       }, {
         label: 'Documentation',
-        click() {
-          shell.openExternal('https://github.com/atom/electron/tree/master/docs#readme');
+        click () {
+          shell.openExternal('https://github.com/atom/electron/tree/master/docs#readme')
         }
       }, {
         label: 'Community Discussions',
-        click() {
-          shell.openExternal('https://discuss.atom.io/c/electron');
+        click () {
+          shell.openExternal('https://discuss.atom.io/c/electron')
         }
       }, {
         label: 'Search Issues',
-        click() {
-          shell.openExternal('https://github.com/atom/electron/issues');
+        click () {
+          shell.openExternal('https://github.com/atom/electron/issues')
         }
       }]
-    }];
-    menu = Menu.buildFromTemplate(template);
-    mainWindow.setMenu(menu);
-
+    }]
+    menu = Menu.buildFromTemplate(template)
+    mainWindow.setMenu(menu)
   }
 
+  ipcMain.on('FileManager', function (event, arg) {
+    var dataJSON = require(path.join(__dirname, 'testfolder', 'data.json'))
 
+    console.log(arg)
 
-  ipcMain.on('FileManager', function(event, arg) {
-
-    var dataJSON = require(path.join(__dirname, "testfolder", "data.json"));
-
-    console.log(arg);
-
-    event.returnValue = dataJSON;
-
-  });
+    event.returnValue = dataJSON
+  })
 
   /*saving*/
-  ipcMain.on('FileSave', function(event, arg) {
-
+  ipcMain.on('FileSave', function (event, arg) {
     if (fs.existsSync('./backend')) {
-      var fileDirectory = "./backend/filesave.py";
-      var indexFolder = "./backend/tagindex.py";
-      var TestFolder = "./testfolder";
+      var fileDirectory = './backend/filesave.py'
+      var indexFolder = './backend/tagindex.py'
+      var TestFolder = './testfolder'
+    } else if (fs.existsSync('./resources/app/backend')) {
+      var fileDirectory = './resources/app/backend/filesave.py'
+      var indexFolder = './resources/app/backend/tagindex.py'
+      var TestFolder = './resources/app/testfolder'
+    } else {
+      var fileDirectory = './Contents/Resources/app/backend/filesave.py'
+      var indexFolder = './Contents/Resources/app/backend/tagindex.py'
+      var TestFolder = './Contents/Resources/app/testfolder'
     }
-    else if (fs.existsSync('./resources/app/backend')){
-      var fileDirectory = "./resources/app/backend/filesave.py";
-      var indexFolder = "./resources/app/backend/tagindex.py";
-      var TestFolder = "./resources/app/testfolder";
-    }
-    else {
-      var fileDirectory = "./Contents/Resources/app/backend/filesave.py";
-      var indexFolder = "./Contents/Resources/app/backend/tagindex.py";
-      var TestFolder = "./Contents/Resources/app/testfolder";
-    }
+    console.log(arg)
+
+    var filesavepy = new PythonShell(fileDirectory)
+    console.log(fileDirectory)
+
+    console.log('BREAK BREAK BREAK')
+
+    var indexShell = new PythonShell(indexFolder)
+    console.log(indexFolder)
+
+    console.log('BREAK BREAK BREAK')
 
 
+    var tags
+    var content
+    var location
+    var filename
 
+    function filesave (tags, content, location, filename) {
+      filesavepy.send(tags)
+      console.log(tags)
+      // content
+      filesavepy.send(content)
+      console.log(content)
+      // folder location
+      filesavepy.send(location)
+      console.log(location)
+      // filename
+      filesavepy.send(filename)
+      console.log(filename)
 
-      console.log(arg);
+      indexShell.send(location)
+      console.log(location)
 
-
-      var filesavepy = new PythonShell(fileDirectory);
-      console.log(fileDirectory);
-
-      console.log("BREAK BREAK BREAK");
-
-
-
-
-      var indexShell = new PythonShell(indexFolder);
-      console.log(indexFolder);
-
-      console.log("BREAK BREAK BREAK");
-
-
-      var tags;
-      var content;
-      var location;
-      var filename;
-
-        function filesave(tags, content, location, filename) {
-          filesavepy.send(tags);
-          console.log(tags);
-          //content
-          filesavepy.send(content);
-          console.log(content);
-          //folder location
-          filesavepy.send(location);
-          console.log(location);
-          //filename
-          filesavepy.send(filename);
-          console.log(filename);
-
-          indexShell.send(location);
-          console.log(location);
-
-          indexShell.send(location);
-          console.log(location);
-          //indexShell.send(location);
-
+      indexShell.send(location)
+      console.log(location)
+      // indexShell.send(location);
     }
 
-    filesave(arg[1], arg[2], TestFolder, arg[0]);
+    filesave(arg[1], arg[2], TestFolder, arg[0])
 
     filesavepy.end(function (err) {
-    if (err) throw err;
-    console.log('finished');
-    });
+      if (err) throw err
+      console.log('finished')
+    })
     indexShell.end(function (err) {
-    if (err) throw err;
-    console.log('finished');
-    });
-
+      if (err) throw err
+      console.log('finished')
+    })
 
     // end the input stream and allow the process to exit
-
-
-  });
-
-
-
-
-});
+  })
+})
