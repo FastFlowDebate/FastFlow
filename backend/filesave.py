b81bf4f286286da3665e506aefd48aba86b2dc61
Adam Towers <adam.towers77@gmail.com>
2016-01-06 08:44:33 -0800

    Pranav's solution?
    this is to try and fix (Issue 9)[https://github.com/Zarkoix/FastFlow/issues/9]

--- backend/filesave.py
+++ backend/filesave.py
@@ -1,7 +1,7 @@
 '''
 This script writes to a file.
 '''
-tags = raw_input("tags: ")
+tags = raw_input("tags: ").split(" ")
 # text goes here
 text = raw_input("text: ")
 #file destination goes here
@@ -12,7 +12,18 @@ filename = raw_input("filename: ")
 file = open(str(destination) + "/" + filename, 'w')
 #deletes previous contents of file
 file.truncate()
+#this line initializes the string that will be entered into the file
+strTag = tag[0]
+#then we iterate through all the tags to add them to the list
+#we skip the first one for formatting reasons
+for tag in tags:
+    #if the tag is the first tag, then it passes
+    if tag == tags[0]:
+        pass
+    #else it will add the tag after a space
+    else:
+        strTag = strTag + " " + tag
 #writes text to file
-file.write("<!--" + tags + "-->\n")
+file.write("<!--" + strTag + "-->\n")
 file.write(text)
 exit()
