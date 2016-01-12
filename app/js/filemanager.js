3229f6211e738a30881d58982882421a36e3f502
Blazerp <pranavseetha@gmail.com>
2016-01-12 09:55:39 -0800

    File Tree
    This is the version of the project that will open up hidden until you
    click on the tags. The concept is a div hiding function, unhide. This
    function is an action that is performed onclick of a button and acts on
    a div with a certain id. We may have to edit the CSS to make it look
    prettier.
    --Pranav

--- app/js/filemanager.js
+++ app/js/filemanager.js
@@ -1,13 +1,34 @@
 const ipcRenderer = require('electron').ipcRenderer
 var path = require('path')
 
+//This function hides a div based on button click
+function unhide(clickedButton, divID) {
+var item = document.getElementById(divID);
+if (item) {
+    if(item.className=='hidden'){
+        item.className = 'unhidden' ;
+        clickedButton.value = 'hide'
+    }else{
+        item.className = 'hidden';
+        clickedButton.value = 'unhide'
+    }
+  }
+}
+
+document.write("<style> .hidden{display:none;} .unhidden{display:block;} </style>")
+
+
 $(document).ready(function () {
   var dataJSON = ipcRenderer.sendSync('FileManager', 'ready')
 
   var theUL = document.createElement("UL")
 
   for (i = 0; i < dataJSON[0].length; i++) {
-    var element = document.createElement("LI")
+    //Changed from UL here
+    var element = document.createElement("UL")
+    var my_string = "unhide(this, '" + i +"')";
+
+    element.setAttribute("onClick", my_string)
 
     var text = document.createTextNode(path.basename(dataJSON[0][i]))
 
@@ -15,7 +36,10 @@ $(document).ready(function () {
 
     theUL.appendChild(element)
 
-    var element2 = document.createElement("UL");
+    var element2 = document.createElement("div");
+    element2.setAttribute('id',i)
+    element2.setAttribute('class', 'hidden')
+
 
     for (j = 0; j < dataJSON[1][i].length; j++){
       var element1 = document.createElement("LI");
