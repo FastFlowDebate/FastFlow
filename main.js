ea00274863780acc96d174917e054b68e7189f3e
Blazerp <pranavseetha@gmail.com>
2016-01-12 12:57:48 -0800

    This is Connecting to my mysql server on my laptop

--- main.js
+++ main.js
@@ -1,5 +1,21 @@
 /* eslint strict: 0 */
 'use strict'
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : '127.0.0.1',
+  user     : 'root',
+  password : 'Pranav100'
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+
+  console.log('connected as id ' + connection.threadId);
+});
+
 
 var path = require('path')
 var fs = require('fs')
