5fc07f502177c23a904ccc9befe8e0f573c58118
Isaac Lo <isaaclo123@gmail.com>
2016-05-23 10:40:24 -0700

    textbox expands, added gray border to bottom of flow

--- app/flow/flow.js
+++ app/flow/flow.js
@@ -1,25 +1,37 @@
-function deleteRow(row){
-  var tbl = document.getElementById('flowspace')
-  if (tbl.rows.length > 1) {
-      tbl.deleteRow(row)
+var Selections = [{
+        name: 'link',
+        buttons: ['linkEdit'],
+        test: AlloyEditor.SelectionTest.link
+    }, {
+        name: 'image',
+        buttons: ['imageLeft', 'imageRight'],
+        test: AlloyEditor.SelectionTest.image
+    }, {
+        name: 'text',
+        buttons: ['bold', 'italic', 'underline', 'link'],
+        test: AlloyEditor.SelectionTest.text
+    }, {
+        name: 'table',
+        buttons: ['tableRow', 'tableColumn', 'tableCell', 'tableRemove'],
+        getArrowBoxClasses: AlloyEditor.SelectionGetArrowBoxClasses.table,
+        setPosition: AlloyEditor.SelectionSetPosition.table,
+        test: AlloyEditor.SelectionTest.table
+}]
+
+$(document).ready(function () {
+  var editor = AlloyEditor.editable('.textbox', {
+    toolbars: {
+      add: {
+        buttons: ['hline', 'table'],
+        tabIndex: 2
+      },
+      styles: {
+        selections: Selections,
+        tabIndex: 1
+      }
     }
-}
-
-function deleteColumn(cell){
-   var tbl = document.getElementById('flowspace')
-   if (tbl.rows[0].cells.length > 1){
-     for (i = 0; i < tbl.rows.length; i++) {
-         tbl.rows[i].deleteCell(cell)
-     }
- }
-
-}
-function clearText(x,y){
-  var theTextBox = document.getElementById(String(x)+"_"+String(y));
-  theTextBox.value = "";;
-
-}
-
+  })
+})
 
 function createCell(cell) {
 
@@ -27,9 +39,10 @@ function createCell(cell) {
 
     var string = "<table><tr><td><a href = \"#\" onclick = deleteColumn(this.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.cellIndex)>x</a></td>";
     string += "<td><a href = \"#\" onclick = this.parentNode.parentNode.parentNode.childNodes[1].childNodes[0].childNodes[0].value=\"\">x</a></td></tr>";
-    string += "<tr><td><textarea></textarea></td>";
+    string += "<tr><td><div contenteditable=\"true\" class=\"textbox\"></div></td>";
     string += "<td><a href = \"#\" onclick = deleteRow(this.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.rowIndex)>x</a></td></tr></table>";
     content.innerHTML = string;
+    $(".content").addClass('textBoxDiv')
     cell.appendChild(content);
 
 }
