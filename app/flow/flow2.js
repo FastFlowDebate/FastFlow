c3a5215646a1f204677d5b86e968cd2c1ab4987d
Zarkoix <adam.towers@me.com>
2016-05-26 23:33:42 -0700

    flow 2 got quite teh upgrade

--- app/flow/flow2.js
+++ app/flow/flow2.js
@@ -1,4 +1,22 @@
 var app = angular.module("flowing", []);
 app.controller("flowController", function($scope) {
-    $scope.flow = [["Milk", "Bread", "Cheese"],["Milk", "Bread", "Cheese"]];
+    $scope.flow = [[{
+      "text": "Racism is bad",
+      "cards": [{
+        "tag": "Yaomomoako",
+        "content": "stuff like racism is bad"
+      },
+      {
+        "tag": "Isaac Lo",
+        "content": "memes are the only option"
+      }]
+    },
+    {
+      "text": "Bad is unjust",
+      "cards": []
+    },
+    {
+      "text": "Thus we negate",
+      "cards": []
+    }]];
 });
