ca3f489b1321e99bab50c38b409a82373cc3a810
Zarkoix <adam.towers@me.com>
2016-05-29 16:00:56 -0700

    added cardReferencer Directive (see top of flow2 for example)

---
+++ app/flow/cardRef.js
@@ -0,0 +1,22 @@
+var app = angular.module("flowing", []);
+app.controller("cardRefController", function($scope) {
+    $scope.flow = [[{
+      "text": "Racism is bad",
+      "cards": [{
+        "tag": "Yaomomoako",
+        "content": "stuff like racism is bad"
+      },
+      {
+        "tag": "Isaac Lo",
+        "content": "memes are the only option"
+      }]
+    },
+    {
+      "text": "Bad is unjust",
+      "cards": []
+    },
+    {
+      "text": "Thus we negate",
+      "cards": []
+    }]];
+});
--- app/flow/flow2.js
+++ app/flow/flow2.js
@@ -1,22 +1,41 @@
-var app = angular.module("flowing", []);
+var app = angular.module("flowing", ['ngSanitize']);
 app.controller("flowController", function($scope) {
-    $scope.flow = [[{
-      "text": "Racism is bad",
-      "cards": [{
-        "tag": "Yaomomoako",
-        "content": "stuff like racism is bad"
-      },
-      {
-        "tag": "Isaac Lo",
-        "content": "memes are the only option"
-      }]
-    },
-    {
-      "text": "Bad is unjust",
-      "cards": []
-    },
-    {
-      "text": "Thus we negate",
-      "cards": []
-    }]];
-});
+	$scope.flow = [
+		[{
+			"text": "Racism is bad",
+			"cards": [{
+				"tag": "Yaomomoako",
+				"content": "stuff like racism is bad"
+			}, {
+				"tag": "Isaac Lo",
+				"content": "memes are the only option"
+			}]
+		}, {
+			"text": "Bad is unjust",
+			"cards": []
+		}, {
+			"text": "Thus we negate",
+			"cards": []
+		}]
+	]
+})
+
+//
+const ipcRenderer = require('electron').ipcRenderer
+
+app.directive('ffcardref', function() {
+	return {
+		restrict: 'E',
+		scope: {
+			title: '@title'
+		},
+		templateUrl: 'flow/cardRef.html',
+		controller: function($scope, $element, $attrs) {
+    	$scope.title = $attrs.title
+    	$scope.content = 'content'
+        var FileArray = ipcRenderer.sendSync('FileOpen', $scope.title)
+        $scope.title = FileArray[0]
+        $scope.content = FileArray[2]
+    }
+	}
+})
