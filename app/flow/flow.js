c0bf40642f70599235537612554eea3e32de0468
Isaac Lo <isaaclo123@gmail.com>
2016-06-05 22:10:58 -0700

    removed test parts on flow, made it clean

--- app/flow/flow.js
+++ app/flow/flow.js
@@ -3,19 +3,7 @@ app.controller("flowController", function($scope) {
 
 	$scope.flow = [
 		[{
-			"text": "Racism is bad",
-			"cards": [{
-				"tag": "Yaomomoako",
-				"content": "stuff like racism is bad"
-			}, {
-				"tag": "Isaac Lo",
-				"content": "memes are the only option"
-			}]
-		}, {
-			"text": "Bad is unjust",
-			"cards": []
-		}, {
-			"text": "Thus we negate",
+			"text": "",
 			"cards": []
 		}]
 	]
