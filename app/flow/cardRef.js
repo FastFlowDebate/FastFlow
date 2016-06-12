012403bb572b6b870c7791c5a349fc8b644b5dbc
Zarkoix <adam.towers@me.com>
2016-06-12 10:13:24 -0700

    card delete button now hides

--- app/flow/cardRef.js
+++ app/flow/cardRef.js
@@ -4,15 +4,14 @@ ngApp.directive('ffcardref', function() {
 	return {
 		restrict: 'E',
 		scope: {
-			title: '='
+			title: '=',
+			deleteFunc: '&'
 		},
 		templateUrl: 'flow/cardRef.html',
 		controller: function($scope, $element, $attrs) {
 			var FileArray = ipcRenderer.sendSync('FileOpen', $scope.title)
 			if (FileArray) {
-				$scope.content = 'memes'
-				var bold = getCardBold(FileArray[2], '...')
-				$scope.content = bold
+				$scope.content = getCardBold(FileArray[2], '...')
 			} else {
 				$scope.content = '<em>Error 404 card not found</em>'
 			}
