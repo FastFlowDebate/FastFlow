0556a32d4a53e1d05df2166d07e1323b5c3e5bec
Zarkoix <adam.towers@me.com>
2016-06-16 00:55:23 -0700

    fixed weird scrolling, beginning of flow card modals

--- app/flow/cardRef.js
+++ app/flow/cardRef.js
@@ -1,17 +1,33 @@
 const ipcRenderer = require('electron').ipcRenderer
-
+//style explanation:
+// 1 - version used in flow, short and has only the bolded text
+// 2 - version for modal, all text maintaining original formatting
 ngApp.directive('ffcardref', function() {
 	return {
 		restrict: 'E',
 		scope: {
 			title: '=',
-			deleteFunc: '&'
+			deleteFunc: '&',
+			style: '='
 		},
 		templateUrl: 'flow/cardRef.html',
 		controller: function($scope, $element, $attrs) {
+			$scope.overlay = function() {
+				var modalEl = document.createElement('div')
+				modalEl.className += " modal"
+				modalEl.innerHtml = '<ffcardref style="2" title="$scope.title" class="modal card mui-panel"></ffcardref>'
+
+					// show modal
+				mui.overlay('on', modalEl)
+			}
+
 			var FileArray = ipcRenderer.sendSync('FileOpen', $scope.title)
 			if (FileArray) {
-				$scope.content = getCardBold(FileArray[2], '...')
+				if($scope.style === 1) {
+					$scope.content = getCardBold(FileArray[2], '...')
+				} else if($scope.style ===2){
+					$scope.content = FileArray[2]
+				}
 			} else {
 				$scope.content = '<em>Error 404 card not found</em>'
 			}
