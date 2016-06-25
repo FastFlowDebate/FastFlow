3b4d9165d80bc8038752c5ed37382a64aaa82a36
Isaac Lo <isaaclo123@gmail.com>
2016-06-24 18:30:55 -0700

    a bit more changes

--- app/cardManager/cardManager.js
+++ app/cardManager/cardManager.js
@@ -7,6 +7,20 @@ module.exports = angular.module('fastflowApp.cardManager', ['ngRoute'])
 	}])
 	.controller('cardManagerCtrl', ['$scope', function($scope) {
 			$scope.dataJSON = ipcRenderer.sendSync('FileManager', 'ready')
+
+			$scope.hideList = Array.apply(null, Array($scope.dataJSON[0].length)).map(function (x, i) { return true });
+
+			$scope.hideToggle = function(x) {
+				console.log($scope.hideList)
+
+				if ($scope.hideList[x] = false) {
+					$scope.hideList[x] = true;
+					console.log("true now")
+				} else if ($scope.hideList[x] = true){
+					$scope.hideList[x] = false;
+					console.log("false now")
+				}
+			};
   }])
 
 /*
