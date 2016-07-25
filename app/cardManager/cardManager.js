9eebddc7c79f012a5b70ba036c3b1c89b555d8aa
Zarkoix <adam.towers@me.com>
2016-07-24 19:12:37 -0700

    Revert "Merge branch 'master' of github.com:FastFlowDebate/FastFlow"
    This reverts commit 095e6da350eb954e4b2cecadc5b624bb53d0a42c, reversing
    changes made to 16cbf5d765ae95489378acea3f862d9831ebf6ab.

---
+++ app/cardManager/cardManager.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+	$('.collapsible').collapsible({
+		accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+	});
+});
+
+module.exports = angular.module('fastflowApp.cardManager', ['ngRoute'])
+	.config(['$routeProvider', function($routeProvider) {
+		$routeProvider.when('/cardManager', {
+			templateUrl: 'cardManager/cardManager.html',
+			controller: 'cardManagerCtrl'
+		})
+	}])
+	.controller('cardManagerCtrl', ['$scope', function($scope) {
+			$scope.dataJSON = ipcRenderer.sendSync('CardManager', 'ready')
+			console.log($scope.dataJSON[1])
+			$scope.tagContent = []
+
+			$scope.URIGenerate = function(name) {
+				console.log('encode: ' + encodeURIComponent(name))
+				return encodeURIComponent(name)
+			}
+  }])
