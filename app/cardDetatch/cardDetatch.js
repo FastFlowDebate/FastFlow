c8563988a130b424a0a7352c31ce14e3018edf08
Adam Towers <adam.towers77@gmail.com>
2016-09-22 22:37:52 -0700

    Card ui (#76)
    * opens in modal at first
    
    * modal done
    
    * cardDetatch
    
    * fixing FAB placement

---
+++ app/cardDetatch/cardDetatch.js
@@ -0,0 +1,25 @@
+module.exports = angular.module('fastflowApp.cardDetatch', ['ngRoute', 'MassAutoComplete', 'ngSanitize', 'angular-medium-editor', 'ngAnimate'])
+	.config(['$routeProvider', function($routeProvider) {
+		$routeProvider.when('/cardDetatch/:tag', {
+			templateUrl: 'cardDetatch/cardDetatch.html',
+			controller: 'cardDetatchCtrl'
+		})
+	}])
+	.controller('cardDetatchCtrl', ['$scope', '$routeParams', function($scope, $routeParams) {
+		if ($routeParams.tag) {
+			var decodedURI = decodeURIComponent($routeParams.tag)
+			card = ipcRenderer.sendSync('FileOpen', decodedURI)
+			if (card == []) console.log('error, card not found')
+			$scope.title = card.tagLine
+			$scope.$parent.setNav({
+				left: [],
+				right: []
+			}, $scope.title)
+			$scope.content = card.content
+			$scope.cite = card.citation
+		} else {
+			console.log('no param, this is an error, go back to safety!')
+			$scope.content = "ERROR"
+			$scope.title = "ERROR"
+		}
+	}])
--- app/cardManager/cardManager.js
+++ app/cardManager/cardManager.js
@@ -13,10 +13,30 @@ module.exports = angular.module('fastflowApp.cardManager', ['ngRoute'])
 	}])
 	.controller('cardManagerCtrl', ['$scope', 'defaultNav', function($scope, defaultNav) {
 		$scope.$parent.setNav(defaultNav, 'Cards')
+		jQuery('.tooltipped').tooltip({delay: 50})
 		$scope.dataJSON = ipcRenderer.sendSync('CardManager', 'ready')
 		$scope.sTags = Object.keys($scope.dataJSON)
 		console.log($scope.sTags)
 		$scope.URIGenerate = function(name) {
 			return encodeURIComponent(name)
+		}
+
+		$scope.openCard = function (cardID) {
+			card = ipcRenderer.sendSync('FileOpen', cardID)
+			if (card == []) console.log('error, card not found')
+			$scope.modalCardTag = cardID
+			$scope.modalCardContent = card.content
+			$scope.modalCardCite = card.citation
+
+			$('#cardModal').openModal()
+		}
+
+		$scope.fullscreen = function () {
+			$('#cardModal').closeModal()
+			window.location.replace('#card/' + $scope.URIGenerate($scope.modalCardTag))
+		}
+		$scope.detatch = function () {
+			$('#cardModal').closeModal()
+			window.open('#cardDetatch/' + $scope.URIGenerate($scope.modalCardTag))
 		}
   }])
