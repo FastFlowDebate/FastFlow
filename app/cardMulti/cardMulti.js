6a073bfc66bb0ec73fa64bffe705dd73a66a30a5
Zarkoix <adam.towers@me.com>
2016-12-09 09:58:26 -0800

    adding getcardbytag function, and function call in cardMulti

--- app/cardMulti/cardMulti.js
+++ app/cardMulti/cardMulti.js
@@ -11,4 +11,14 @@ module.exports = angular.module('fastflowApp.cardMulti', ['ngRoute'])
 	}])
 	.controller('cardMultiCtrl', ['$scope', '$routeParams', function($scope, $routeParams) {
 		$scope.x = 'y'
+		if ($routeParams.tag) {
+			var decodedURI = decodeURIComponent($routeParams.tag)
+			console.log('fetching cards with tag: ' + decodedURI)
+			$scope.multicard = ipcRenderer.sendSync('OpenCards', decodedURI)
+			if ($scope.multicard == []) console.log('error, card not found')
+			console.log($scope.multicard)
+		} else {
+			console.log('no param, this is an error, go back to safety!')
+			$scope.error = true
+		}
   }])
