060f2160c0f9f10d711e844c1f7931b285486bbe
Adam Towers <adam.towers77@gmail.com>
2017-01-21 10:58:24 -0800

    webpack now minifies

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -1,18 +1,41 @@
 // webpack.config.js
 var webpack = require('webpack')
+var webpackUglifyJsPlugin = require('webpack-uglify-js-plugin')
+var path = require('path')
+
 
 module.exports = {
-  context: __dirname + '/src',
+  context: __dirname + '',
   entry: './app.js',
-
   output: {
     filename: 'bundle.js',
     path: __dirname + '/build'
   },
-
-  module: {
-    loaders: [
-      {  }
-    ]
-  }
-};
+  plugins: [
+    new webpackUglifyJsPlugin({
+      cacheFolder: path.resolve(__dirname, 'public/cached_uglify/'),
+      debug: true,
+      minimize: true,
+      sourceMap: false,
+      output: {
+        comments: false
+      },
+      compressor: {
+        warnings: false
+      }
+    })
+  ],
+  externals: [
+    (function () {
+      var IGNORES = [
+        'electron'
+      ];
+      return function (context, request, callback) {
+        if (IGNORES.indexOf(request) >= 0) {
+          return callback(null, "require('" + request + "')");
+        }
+        return callback();
+      };
+    })()
+  ]
+}
